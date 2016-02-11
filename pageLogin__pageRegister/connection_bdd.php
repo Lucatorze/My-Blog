e4a505f782e09417824591ef0e31554b5977cfbc
sylvainSUPINTERNET <sylvain.joly00@orange.fr>
2016-02-11 14:01:21 +0100

    suppresion fichier
    bdd conf

--- pageLogin__pageRegister/connection_bdd.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-$config = [
-    "host" => "mysql:host=127.0.0.1;dbname=blogbdd",
-    "user" => "root",
-    "password" => ""
-];
-session_start();
-$pdo = new PDO($config['host'],$config['user'],$config['password']);
